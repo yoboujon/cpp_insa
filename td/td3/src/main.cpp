779d52bbf5d38d34ff4a4b0f65edf83d2cb5f0e1
Yohan Boujon <yo.boujon@gmail.com>
2023-10-03 13:48:51 +0200

    TD 3 Question 1 and 2.

--- td/td3/src/main.cpp
+++ td/td3/src/main.cpp
@@ -1,8 +1,24 @@
 #include "../include/vector.h"
+#include "../include/smart_vector.h"
 #include <iostream>
 
 int main(void)
 {
-    std::cout << "Hello TD3 !" << std::endl;
+    Smart_Vector<int> smartInt, test(10,6);
+    for(int i=0; i<6; i++)
+        smartInt.PushBack(i);
+
+    std::cout << smartInt << std::endl;
+    std::cout << test << std::endl; 
+
+    std::cout << smartInt + test << std::endl;
+    std::cout << smartInt + smartInt << std::endl;
+
+    Smart_Vector<int> little(5,1), big(10,5);
+    little += big;
+    std::cout << little << std::endl;
+    little = Smart_Vector<int>(5,1);
+    big += little;
+    std::cout << big << std::endl;
     return 0;
 }
