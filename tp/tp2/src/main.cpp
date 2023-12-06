423441a2c07a67a74662f639328b6bb2e7c282a2
Yohan Boujon <yo.boujon@gmail.com>
2023-12-06 21:01:00 +0100

    TP2, Exercice 2.

--- tp/tp2/src/main.cpp
+++ tp/tp2/src/main.cpp
@@ -1,17 +1,38 @@
 #include <cstddef>
 #include <iostream>
+#include <iterator>
 #include <sys/types.h>
 #include <vector>
+#include <list>
 
-void question1(uint maxVec);
+void question1(const uint maxVec);
+void question2(const uint max);
 
 int main(void)
 {
     question1(10);
+    question2(10);
     return 0;
 }
 
-void question1(uint maxVec)
+void question2(const uint max)
+{
+    std::list<char> l1;
+    for(uint i=0; i<max; i++)
+        l1.push_back('a'+i);
+    auto l2 = l1;
+    l2.erase(l2.begin(),std::next(l2.begin(), 5));
+
+    // printing
+    for(const auto& val : l1)
+        std::cout << val << "\t";
+    std::cout << std::endl;    
+    for(const auto& val : l2)
+        std::cout << val << "\t";
+    std::cout << std::endl; 
+}
+
+void question1(const uint maxVec)
 {
     std::vector<float> v1;
     const float value = 1.0f/maxVec;
