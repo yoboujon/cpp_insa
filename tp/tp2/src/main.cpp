709e3d3b46be6f732aeea2062674f1972c48d23a
Yohan Boujon <yo.boujon@gmail.com>
2023-12-06 20:35:10 +0100

    TP2, Exercice 1.

--- tp/tp2/src/main.cpp
+++ tp/tp2/src/main.cpp
@@ -1,7 +1,31 @@
+#include <cstddef>
 #include <iostream>
+#include <sys/types.h>
+#include <vector>
+
+void question1(uint maxVec);
 
 int main(void)
 {
-    std::cout << "Hello TP2 !" << std::endl;
+    question1(10);
     return 0;
+}
+
+void question1(uint maxVec)
+{
+    std::vector<float> v1;
+    const float value = 1.0f/maxVec;
+    for(uint i=0; i<maxVec; i++)
+        v1.push_back(value*i);
+    auto v2 = v1;
+    for(size_t i=0; i<(maxVec/2); i++)
+        v2.pop_back();
+
+    // printing
+    for(const auto& val : v1)
+        std::cout << val << "\t";
+    std::cout << std::endl;    
+    for(const auto& val : v2)
+        std::cout << val << "\t";
+    std::cout << std::endl;  
 }
